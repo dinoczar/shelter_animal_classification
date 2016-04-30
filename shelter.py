4d45488ac7d453f7ab28cf287dd79e58a50521a7
chris <chris.sj.liu@gmail.com>
2016-04-29 19:40:06 -0700

    Added functions to better deal with color and breed; Added code for ensemble

--- shelter.py
+++ shelter.py
@@ -7,8 +7,6 @@ Created on Tue Mar 22 16:51:49 2016
 
 ##############################################################################
 ##############################################################################
-#code for querying animals that have a name and have age less than 500 days
-#df[df['Has_Name'] == True][df[df['Has_Name'] == True]['AgeinDays'] <500]
 
 #Useful Functions
 
@@ -29,6 +27,11 @@ def has_name(name):
         return True
     else:
         return False
+
+def is_mix(breed):
+    if breed.find('Mix') >= 0 or len(breed.split('/')) > 1:
+        return True
+    return False
 
 
 def convert_age(s):
@@ -83,6 +86,23 @@ def get_gender(gender):
     if gender.find('Female') >= 0: return 'female'
     return 'unknown'
 
+def get_color(color):
+    color = str(color)
+    if color.find('White')>=0: return 'white'
+    if color.find('Black')>=0: return 'black'
+    if color.find('Brown')>=0: return 'brown'
+    if color.find('Tabby')>=0: return 'tabby'
+    if color.find('Tan')>=0: return 'tan'
+    if color.find('Blue')>=0: return 'blue'
+    if color.find('Orange')>=0: return 'orange'
+    if color.find('Brindle')>=0: return 'brindle'
+    if color.find('Red')>=0: return 'red'
+    if color.find('Tricolor')>=0: return 'tricolor'
+    if color.find('Tortie')>=0: return 'tortie'
+    if color.find('Cream')>=0: return 'cream'
+    if color.find('Point') >= 0: return 'point'
+    if color.find('Calico')>=0: return 'calico'
+    return 'n/a'
 
 def clean(df, avg_age = None):
     df['Has_Name'] = [has_name(name) for name in df.Name]
@@ -99,29 +119,26 @@ def clean(df, avg_age = None):
     df['Day'] = [day(d.split()[0]) for d in df.DateTime]
     df['Gender'] = df.SexuponOutcome.apply(get_gender)
     df['Neutered'] = df.SexuponOutcome.apply(get_neutered)
-    df['Color'] = [', '.join(c.split('/')) for c in df.Color]
+    df['Mix'] = df.Breed.apply(is_mix)    
     return df
 
-def preprocess(df, breed_vec, color_vec, feature_vec):
-    breed_mat = breed_vec.transform(df.pop('Breed'))
-    color_mat = color_vec.transform(df.pop('Color'))
-    X = feature_vec.transform(df.T.to_dict().values())
+def preprocess(df, color_vec, feature_vec, fit = False):
+    if fit == True:
+        #breed_mat = breed_vec.fit_transform(df.pop('Breed'))
+        color_mat = color_vec.fit_transform(df.pop('Color'))
+        X = feature_vec.fit_transform(df.T.to_dict().values())
+    else:
+        #breed_mat = breed_vec.transform(df.pop('Breed'))
+        color_mat = color_vec.transform(df.pop('Color'))
+        X = feature_vec.transform(df.T.to_dict().values())
     from scipy.sparse import hstack
-    X = hstack((X, color_mat, breed_mat))
+    X = hstack((X, color_mat))
     return X.toarray()
 
-
-#############################################################################
-
-
-
-#############################################################################
-def main():    
-    #LOAD DATA AND PROCESS DATA
-    import timeit
-    start = timeit.default_timer()    
-    
+def load_data():
     df = pd.read_csv('train.csv')
+    
+    
     df = clean(df)
     mean_age = int(df.AgeinDays.mean())
     
@@ -132,41 +149,20 @@ def main():
     #drop all unnecessary columns
     df = df.drop(['AnimalID','Name','OutcomeSubtype', 'DateTime', 
                   'AgeuponOutcome', 'SexuponOutcome'], axis = 1)
-    ID = test_df.pop('ID')
+    
+    
     test_df = test_df.drop(['Name','AgeuponOutcome', 
                             'SexuponOutcome'], axis = 1)
-    
-    #set target as y
-    y = df.pop('OutcomeType')
-    y_vec = CountVectorizer()
-    
-    
-    
-    #throw breed and color to countvectorizers
-    breed_vec = CountVectorizer(tokenizer = tokenize)
-    breed_vec.fit(df.Breed)
-    color_vec = CountVectorizer(tokenizer = tokenize)
-    color_vec.fit(df.Color)
-    
-    #throw everything else to dictvectorizer
-    feature_vec = DictVectorizer()
-    feature_vec.fit(df.T.to_dict().values())
-    
-    
-    X = preprocess(df, breed_vec, color_vec, feature_vec)
-    X_test = preprocess(test_df, breed_vec, color_vec, feature_vec)
-    
-    #from sklearn.ensemble import RandomForestClassifier
-    #clf = RandomForestClassifier(n_estimators = 500)
-    
-    from sklearn.ensemble import GradientBoostingClassifier
-    clf = GradientBoostingClassifier(learning_rate = 0.05,
-                                     n_estimators = 750,
-                                     max_depth = 6)    
-    
-    '''
-    #CODE FOR ESTIMATING THE LOG LOSS OF TRAINING DATA
+                            
+                            
+    df = df.drop('Breed', axis=1)    
+    test_df = test_df.drop('Breed', axis = 1)
+    return df, test_df
+
+def estimate_loss(X, y, X_test, clf):
+    #CODE FOR ESTIMATING THE LOG LOSS OF TRAINING DATA        
     cut = int(len(y)*0.8)
+    y_vec = CountVectorizer()
     X_train = X[:cut]
     X_val = X[cut:]
     y_train = y[:cut]
@@ -180,13 +176,78 @@ def main():
     from sklearn.metrics import log_loss
     
     print([log_loss(y_mat[cut:], results)])
-    '''
+
+
+#############################################################################
+
+
+
+#############################################################################
+def main():
+    #LOAD DATA AND PROCESS DATA
+    import timeit
+    start = timeit.default_timer()    
     
-    clf.fit(X,y)
-    results = clf.predict_proba(X_test)
-    result_df = pd.DataFrame(data = results, columns = clf.classes_)
+    print('Loading data...')
+    df, test_df = load_data()
+    ID = test_df.pop('ID')
+    
+    
+    print('Preprocessing data...')    
+    #set target as y
+    y = df.pop('OutcomeType')
+    
+    #throw breed and color to countvectorizers
+    #breed_vec = CountVectorizer(tokenizer = tokenize)
+    color_vec = CountVectorizer(tokenizer = tokenize)
+    
+    #throw everything else to dictvectorizer
+    feature_vec = DictVectorizer()
+    
+    
+    X = preprocess(df, color_vec, feature_vec, fit = True)
+    X_test = preprocess(test_df, color_vec, feature_vec)
+    
+    del df
+    del test_df
+    
+    print('Training classifier...')
+    from sklearn.ensemble import RandomForestClassifier
+    rfc = RandomForestClassifier(n_estimators = 1250)    
+    
+    
+    #Use gradient boosting classifier as base
+    from sklearn.ensemble import GradientBoostingClassifier
+    gbc = GradientBoostingClassifier(learning_rate = 0.05,
+                                     n_estimators = 1000,
+                                     max_depth = 6)    
+    
+    
+    gbc.fit(X,y)
+    print('Making predictions...')
+    results = gbc.predict_proba(X_test)
+    
+    #Create an ensemble using a number of random forest classifiers
+    N = 25
+    for i in range(N):
+        print(str(i)+' Training classifier...')
+        rfc.fit(X,y)
+        results+= rfc.predict_proba(X_test)        
+    
+    #Give the  gradient boosting classifier more votes in the ensemble 
+    results = results/(N+5)
+    
+    print('Writing results to file...')
+    result_df = pd.DataFrame(data = results, columns = gbc.classes_)
     result_df.insert(0, 'ID', ID)
-    result_df.to_csv('submission.csv', index = False)
+    result_df.to_csv('gbc_rf_201600413.csv', index = False)
+    
+    
+    
+    
     
     stop = timeit.default_timer()
-    print(stop - start)
+    print('Execution time: %.2f minutes' %((stop - start)/60))
+
+if __name__ == '__main__':
+    main()
